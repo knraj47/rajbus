e00d9893f4c80bce17468b38f6801447ea2a5242
vishnuchinthala <vishnuchinthala5462@gmail.com>
2022-04-11 16:08:46 +0530

    added mysql dependancy

--- src/main/java/com/travels/rajbus/RajbusApplication.java
+++ src/main/java/com/travels/rajbus/RajbusApplication.java
@@ -1,9 +1,11 @@
 package com.travels.rajbus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class RajbusApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/travels/rajbus/User.java
+++ src/main/java/com/travels/rajbus/User.java
@@ -1,102 +1,110 @@
 package com.travels.rajbus;
 
-import java.util.List;
 
-import javax.management.relation.Role;
-import javax.persistence.CascadeType;
+import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
 
+@Entity
+@Table
+public class User {
 	
-	@Entity
-	@Table(name = "users")
-	public class User {
-	    @Id
-	    @GeneratedValue(strategy = GenerationType.AUTO)
-	    private Long id;
-	    private String email;
-	    private String firstname;
-	    private String lastname;
-	    private String phoneNumber;
-
-	    private List<Role> roles;
-	    private boolean isActivated;
-	    private String activationHash;
-	    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-	    
-
-	    public String getEmail() {
-	        return email;
-	    }
-
-	    public void setEmail(String email) {
-	        this.email = email;
-	    }
-
-	    public Long getId() {
-	        return id;
-	    }
-
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
-
-	    public String getFirstname() {
-	        return firstname;
-	    }
-
-	    public void setFirstname(String firstname) {
-	        this.firstname = firstname;
-	    }
-
-	    public String getLastname() {
-	        return lastname;
-	    }
-
-	    public void setLastname(String lastname) {
-	        this.lastname = lastname;
-	    }
-
-	    public String getPhoneNumber() {
-	        return phoneNumber;
-	    }
-
-	    public void setPhoneNumber(String phoneNumber) {
-	        this.phoneNumber = phoneNumber;
-	    }
-
-	    public boolean isActivated() {
-	        return isActivated;
-	    }
-
-	    public void setActivated(boolean activated) {
-	        isActivated = activated;
-	    }
-
-	    public List<Role> getRoles() {
-	        return roles;
-	    }
-
-	    public void setRoles(List<Role> roles) {
-	        this.roles = roles;
-	    }
-
-	    public String getActivationHash() {
-	        return activationHash;
-	    }
-
-	    public void setActivationHash(String activationHash) {
-	        this.activationHash = activationHash;
-	    }
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(unique=true)
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	
+	@Column(unique=true)
+	private String Email;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", firstName=" + firstName
+				+ ", lastName=" + lastName +" ,phoneNumber=" + phoneNumber + ", Email=" + Email + "]";
+	}
+	public User(long id, String userName, String password, String firstName, String lastName, String phoneNumber,String email) {
+		
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		Email = email;
+	}
+	public User() {
+		super();
+	
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(Email, firstName, id, lastName, password,phoneNumber, userName);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(Email, other.Email) && Objects.equals(firstName, other.firstName) && id == other.id
+				&& Objects.equals(lastName, other.lastName) && Objects.equals(password, other.password)&& Objects.equals(phoneNumber, other.phoneNumber)
+				&& Objects.equals(userName, other.userName);
 	}
 
-
-
-
+	
+}
